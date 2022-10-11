c11a1153d7313a0f63e65774af47f36fa21e1232
GSeynave <54280059+GSeynave@users.noreply.github.com>
2022-10-11 16:20:22 +0200

    WIP

--- src/app/components/farm/farm.component.spec.ts
+++ src/app/components/farm/farm.component.spec.ts
@@ -22,4 +22,28 @@ describe('LifebarComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should clear interval and start new farm on teleport id changes'), () => {
+  }
+
+  it('should not init farm if zoneTeleport id is 0'), () => {
+  }
+
+  it('should change farm zone if zoneTeleport id changes and is not 0'), () => {
+  }
+
+  it('should change personnage caracteristique if personnage changs'), () => {
+  }
+
+  it('should get a random monster on getMonstreRandom()'), () => {
+  }
+
+  it('should change html vieMonstre'), () => {
+  }
+
+  it('should call getDrop & getMonstreRandom on vieMonstre <= 0'), () => {
+  }
+
+  it('should instanciate new Monstre && new Statistique && interval on clearFarm()'), () => {
+  }
 });
