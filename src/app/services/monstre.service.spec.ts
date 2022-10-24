47e48c053e612db995ed34a8de5a4e59f694f92a
GSeynave <54280059+GSeynave@users.noreply.github.com>
2022-10-24 18:20:51 +0200

    unit test monstres service

--- src/app/services/monstre.service.spec.ts
+++ src/app/services/monstre.service.spec.ts
@@ -1,4 +1,5 @@
 import { TestBed } from '@angular/core/testing';
+import { Monstre } from '../models/monstre';
 
 import { MonstreService } from './monstre.service';
 
@@ -12,5 +13,11 @@ describe('MonstreService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should return list of monstres', () => {
+    let monstres: Monstre[] = [];
+    service.getMonstresByZoneId(1).subscribe((data) => (monstres = data));
+    expect(monstres.length).toBeGreaterThan(0);
   });
 });
